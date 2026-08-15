2ae6c978e2a73f99d24f7d52af3c56ec814b55bd
Aman Usmani <amanusmani3sep@gmail.com>
2026-08-15 07:59:21 +0530

    Time: 1 ms (100%), Space: 133.5 MB (34.56%) - LeetHub

--- 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.java
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.java
@@ -3,40 +3,24 @@ class Solution {
         int i=0;
         int n=0;
         int j=0;
-        int maxl=Integer.MIN_VALUE;
+        int maxl=0;
         while(i<nums.length){
             n=n^nums[i];
             i+=1;
         }
-         if(n!=0){
-                  maxl=nums.length;
+        if(n!=0){
+                 return  maxl=nums.length;
                 
-         }
-        else{
-            for(j=0;j<nums.length;j++){
-                n=n^nums[j];
-                if(n!=0){
+        }
+        for(j=0;j<nums.length;j++){
+            if((n^nums[j])!=0){
                   maxl=nums.length-1;
                   break;
-                }
-                else{
-                      n=n^nums[j];
                 }
           }
           
 
-        }
-        // while(n<=0 && j<nums.length){
-        //        n=n^nums[j];
-        //        j+=1;
-        //        if(n!=0){
-        //         maxl=Math.max(maxl,i-j);
-        //        }
-        // }
-            // maxl=Math.max(maxl,i-j+1);
-        if(maxl==-2147483648){
-            return 0;
-        }
+        
         return maxl;
     }
 }
