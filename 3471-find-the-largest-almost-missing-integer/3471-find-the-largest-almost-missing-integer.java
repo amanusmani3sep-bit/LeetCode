5a6c113d3f2c02764315cbd368b5d2901fcfaef8
Aman Usmani <amanusmani3sep@gmail.com>
2026-08-18 17:59:12 +0530

    Time: 1 ms (99.3%), Space: 44.5 MB (83.51%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,38 +1,42 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int maxn=-1;
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-           System.out.println(nums[i] + " " + map.get(nums[i]));
-          }
-        if(k==1){
-          for(int i=0;i<nums.length;i++){
-            if(map.get(nums[i])==1){
-                maxn=Math.max(maxn,nums[i]);
-                System.out.println(maxn);
-            }
-          }
-            return maxn;
+        int n = nums.length;
+        int[] freq = new int[51];
+
+        for (int num : nums) {
+            freq[num]++;
         }
-        else if(k==nums.length){
-            for(int i=0;i<nums.length;i++){
-                maxn=Math.max(maxn,nums[i]);
-          }
-          return maxn;
+        if (k == 1) {
+            int ans = -1;
+
+            for (int num : nums) {
+                if (freq[num] == 1) {
+                    ans = Math.max(ans, num);
+                }
+            }
+
+            return ans;
         }
-        else{
-            if(map.get(nums[nums.length-1])==1 && map.get(nums[0])==1){
-                return Math.max(nums[nums.length-1],nums[0]);
+        if (k == n) {
+            int ans = -1;
+
+            for (int num : nums) {
+                ans = Math.max(ans, num);
             }
-            else if(map.get(nums[nums.length-1])==1 && map.get(nums[0])!=1){
-                return nums[nums.length-1];
-            }
-            else if(map.get(nums[nums.length-1])!=1 && map.get(nums[0])==1){
-                return nums[0];
-            }
-            
+
+            return ans;
         }
-        return -1;
+
+        int ans = -1;
+
+        if (freq[nums[0]] == 1) {
+            ans = Math.max(ans, nums[0]);
+        }
+
+        if (freq[nums[n - 1]] == 1) {
+            ans = Math.max(ans, nums[n - 1]);
+        }
+
+        return ans;
     }
 }
