a7ef911c637dedbf5bb6a3c0930bd12fe4929157
Aman Usmani <amanusmani3sep@gmail.com>
2026-09-05 07:47:19 +0530

    Time: 5 ms (66.41%), Space: 126.6 MB (71.37%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -2,22 +2,24 @@ class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int maxi=Integer.MIN_VALUE;
         int mini=Integer.MAX_VALUE;
-        HashMap<Integer,Integer> map1=new HashMap<>();
-        HashMap<Integer,Integer> map2=new HashMap<>();
+        int[] map1=new int[nums.length];
+        int[] map2=new int[nums.length];
+        // HashMap<Integer,Integer> map1=new HashMap<>();
+        // HashMap<Integer,Integer> map2=new HashMap<>();
         for (int i=0;i<nums.length;i++){
              if(nums[i]>maxi){
                 maxi=nums[i];
              }
-             map1.put(i,maxi);
+             map1[i]=maxi;
         }
         for (int i=nums.length-1;i>=0;i--){
              if(nums[i]<mini){
                 mini=nums[i];
              }
-             map2.put(i,mini);
+             map2[i]=mini;
         }
         for(int i=0;i<nums.length;i++){
-            if((map1.get(i)-map2.get(i))<=k){
+            if((map1[i]-map2[i])<=k){
                 return i;
             }
         }
