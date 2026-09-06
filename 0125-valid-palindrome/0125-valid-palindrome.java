c41aaf94547d5e18adf3ab48c1413a08dc1c1f46
Aman Usmani <amanusmani3sep@gmail.com>
2026-09-06 20:08:35 +0530

    Time: 2 ms (99.3%), Space: 44.3 MB (73.47%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            while(i<=j &&!Character.isLetterOrDigit(s.charAt(i))){
+                i+=1;
+            }
+            while(i<=j && !Character.isLetterOrDigit(s.charAt(j))){
+                j-=1;
+            }
+            
+        //  if (!Character.isLetterOrDigit(s.charAt(i))) {
+        //      i+=1;
+        //  }
+        //  if (!Character.isLetterOrDigit(s.charAt(j))) {
+        //      j-=1;
+        //  }
+            if (i <= j && (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))) 
+             {
+              return false;
+                     }
+             i+=1;
+             j-=1;
+        }
+        return true;
+    }
+}
